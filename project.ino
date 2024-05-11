f19ba14bc29ba3311ca4d0f5ceaf0854618732e6
Heidi Putkuri <59087733+heksaani@users.noreply.github.com>
2024-05-11 21:48:19 +0300

    Update project.ino
    Make some correction to the fan activation so that the error rate is taken into account

--- project.ino
+++ project.ino
@@ -91,7 +91,7 @@ void loop() {
 
 
   // Increase fan speed if temperature or humidity is too high
-  if (temperature > 23 || humidity > 96) {
+  if (temperature > 22 || humidity > 94) {
     // Increase fan speed
     analogWrite(fanPin1, 100); 
   }
